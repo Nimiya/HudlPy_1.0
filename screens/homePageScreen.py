73b427eff75dee84aec2bafca7e1e16969e9a92b
Nimiya Joseph <nimiyajoseph@gmail.com>
2021-11-23 00:23:57 +0530

    Update homePageScreen.py

--- screens/homePageScreen.py
+++ screens/homePageScreen.py
@@ -6,7 +6,7 @@ class HomePageElements(object):
     PASS = '#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input'
     SUBMIT = '#passwordNext > content > span'
     LOGIN_ERROR = '#password > div.LXRPh > div.dEOOab.RxsGPe'
-    homepage_hudlLogo_img = '//a[contains(@data-qa-id,"site-logo000")]'
+    homepage_hudlLogo_img = '//a[contains(@data-qa-id,"site-logo")]'
     homepage_highSchool_lnk = '//a[contains(text(),"High School, Club & College")]'
     homePage_division_lnk = '//a[contains(text(),"Division")]'
     homePage_support_lnk = '//a[contains(text()," Support")]'
